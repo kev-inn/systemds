57b6cfad662db4c90921a26e5030261170f39ae9
Sebastian <baunsgaard@tugraz.at>
2020-03-19 13:12:36 +0100

    add wheel package

--- src/main/python/create_python_dist.py
+++ src/main/python/create_python_dist.py
@@ -16,7 +16,6 @@
 # ------------------------------------------------------------------------------
 import subprocess
 subprocess.run("python3 pre_setup.py",shell=True, check=True)
-subprocess.run("python3 setup.py sdist",shell=True, check=True)
-# subprocess.run("python3 setup.py sdist bdist\_wheel",shell=True, check=True)
+subprocess.run("python3 setup.py sdist bdist_wheel",shell=True, check=True)
 # post_setup.py moves the files from dist to target which we probably don't want for uploading them to pypi
 #subprocess.run(["python3", "post_setup.py"]).check_returncode()
